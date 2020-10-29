5b1486762e0c974afdab5f7afd184a2ed43f3dc2
Dabbour M <anamejust947@gmail.com>
2020-10-29 18:25:44 +0100

    created login.ejs + route

--- app.js
+++ app.js
@@ -16,9 +16,19 @@ mongoose.connect(MongoDbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+// URL Parser
+app.use(express.urlencoded({extended: true}));
+
+// Routes
 app.get('/', (req, res) => {
     res.render('welcome');
 });
+
+app.post('/users/login', (req, res)=>{
+    console.log(req.body.email);
+    console.log(req.body.password);
+    res.send('success');
+})
 
 const PORT = process.env.PORT | 3000
 app.listen(3000, () => console.log(`Server started on port: ${PORT}`));
