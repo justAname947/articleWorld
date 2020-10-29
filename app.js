23b3c19a16e239754090a7f79c1b50a22bb5bbc6
Dabbour M <anamejust947@gmail.com>
2020-10-29 16:05:57 +0100

    set up ejs engine / layouts

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+
+// Express layouts middleware + setting EJS as view engine
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('test worked')
-})
+    res.render('welcome');
+});
 
 const PORT = process.env.PORT | 3000
 app.listen(3000, () => console.log(`Server started on port: ${PORT}`));
